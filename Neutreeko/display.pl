c0c2eebe441ba33f3a054af134afe7d232455317
Francisco Filipe <francilipe.7@gmail.com>
2018-10-18 12:52:05 +0100

    Added indexes at the bottom and left side of the board

--- Neutreeko/display.pl
+++ Neutreeko/display.pl
@@ -10,19 +10,24 @@ symbol(black, S):- S=' o '.
 symbol(white, S):- S=' x '.
 
 display_game(Board, Player) :- 
+    nl,
     board(Board), 
-    show_board(Board), 
+    show_board(Board, 5), 
     format('~n~nNext player to move: ~d', Player).
 
-show_board([Head | Tail]):-
-    write('+---+---+---+---+---+'),
+show_board([Head | Tail], I):-
+    write(' +---+---+---+---+---+'),
     nl,
+    write(I),
     write('|'),
     display_line(Head),
     nl,
-    show_board(Tail).
-show_board([_]):-
-    write('+---+---+---+---+---+').
+    NI is I - 1,
+    show_board(Tail, NI).
+show_board([_], I):-
+    write(' +---+---+---+---+---+'),
+    nl,
+    write('   A   B   C   D   E').
 
 display_line([Head | Tail]):-
     symbol(Head, S),
