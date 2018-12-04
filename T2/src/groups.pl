fcc4a624574c6c0af8e46c0f9e8990c293eb8030
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2018-12-04 20:12:19 +0000

    We only need to know the number of themes.

--- T2/src/groups.pl
+++ T2/src/groups.pl
@@ -8,20 +8,30 @@
  * groups(+Students, +Proj1Themes, +Proj2Themes, +GroupSize, +PreviousUCsInfo, -Proj1Groups, -Proj2Groups)
  * 
  * Students - list of students (name/id and their gpa)
- * Proj1Themes - list of available themes for the first project
+ * NumThemes1 - number of available themes for the first project
+ * NumThemes2 - number of available themes for the second project
  * GroupSize - interval representing the possible group sizes
- * Proj2Themes - list of available themes for the second project
  * PreviousUCsInfo - list of students that have worked together before
  * Proj1Groups - list of groups for the first project
  * Proj2Groups - list of groups for the second project
  */ 
-groups(Students, Proj1Themes, Proj2Themes, GroupSize, PreviousUCsInfo, Proj1Groups, Proj2Groups).
+groups(Students, NumThemes1, NumThemes2, GroupSize, PreviousUCsInfo, Proj1Groups, Proj2Groups).
 
-main(CWD, StudentsFile, Proj1ThemesFile, Proj2ThemesFile, PreviousUCsInfoFile, GroupSize):-
+/**
+ * main(+CWD, +StudentsFile, +NumThemes1, +NumThemes2, +PreviousUCsInfoFile, +GroupSize)
+ * 
+ * CWD - current working directory
+ * StudentsFile - students file path relative to cwd
+ * NumThemes1 - number of available themes for the first project
+ * NumThemes2 - number of available themes for the second project
+ * PreviousUCsInfoFile - previousUCsInfo file path relative to cwd
+ * GroupSize - interval representing the possible group sizes
+ */ 
+main(CWD, StudentsFile, NumThemes1, NumThemes2, PreviousUCsInfoFile, GroupSize):-
     current_directory(_, CWD),
-    read_files(StudentsFile, Proj1ThemesFile, Proj2ThemesFile, PreviousUCsInfoFile, Students, Proj1Themes, Proj2Themes, PreviousUCsInfo),
-    groups(Students, Proj1Themes, Proj2Themes, GroupSize, PreviousUCsInfo, Proj1Groups, Proj2Groups).
-
+    read_files(StudentsFile, PreviousUCsInfoFile, Students, PreviousUCsInfo),
+    groups(Students, NumThemes1, NumThemes2, GroupSize, PreviousUCsInfo, Proj1Groups, Proj2Groups),
+    write_files(Proj1Groups, Proj2Groups).
 
 /**
  * haveWorkedTogether(+Student1, +Student2, +PreviousUCsInfo)
--- T2/src/io.pl
+++ T2/src/io.pl
@@ -1,34 +1,40 @@
-read_files(StudentsFile, Proj1ThemesFile, Proj2ThemesFile, PreviousUCsInfoFile, Students, Proj1Themes, Proj2Themes, PreviousUCsInfo):-
+/**
+ * read_files(+StudentsFile, +PreviousUCsInfoFile, -Students, -PreviousUCsInfo)
+ * 
+ * StudentsFile - students file name relative to cwd
+ * PreviousUCsInfoFile - previous UCs info file name relative to cwd
+ * Students - students information (ID and GPA)
+ * PreviousUCsInfo - information regarding previous groups in other UCs
+ */ 
+read_files(StudentsFile, PreviousUCsInfoFile, Students, PreviousUCsInfo):-
     open(StudentsFile, read, Str),
-    read_students(Str, Students),
+    read_file(Str, Students),
     close(Str),
-    open(Proj1ThemesFile, read, Str2),
-    read_themes(Str2, Proj1Themes),
-    close(Str2),
-    open(Proj2ThemesFile, read, Str3),
-    read_themes(Str3, Proj2Themes),
-    close(Str3),
-    open(PreviousUCsInfoFile, read, Str4),
-    read_students(Str4, PreviousUCsInfo),
-    close(Str4).
+    open(PreviousUCsInfoFile, read, Str2),
+    read_file(Str2, PreviousUCsInfo),
+    close(Str2).
 
-read_themes(Stream,[]) :-
+/**
+ * read_file(+Stream, -List)
+ * 
+ * Stream - file stream
+ * List - list of pairs (students/previousUCsInfo)
+ */ 
+read_file(Stream,[]) :-
     at_end_of_stream(Stream).
 
-read_themes(Stream,[X|L]) :-
-    \+ at_end_of_stream(Stream),
-    read(Stream,X),
-    read_themes(Stream,L).
-
-read_students(Stream,[]) :-
-    at_end_of_stream(Stream).
-
-read_students(Stream,[[X, Y]|L]) :-
+read_file(Stream,[[X, Y]|L]) :-
     \+ at_end_of_stream(Stream),
     read(Stream,X),
     read(Stream,Y),
-    read_students(Stream,L).
+    read_file(Stream,L).
 
+/**
+ * write_files(+Proj1Groups, +Proj2Groups)
+ * 
+ * Proj1Groups - list of the groups for the first project
+ * Proj2Groups - list of the groups for the second project
+ */ 
 write_files(Proj1Groups, Proj2Groups):-
     open('proj1groups.txt', write, Str),
     write_groups(Str, Proj1Groups),
@@ -37,13 +43,24 @@ write_files(Proj1Groups, Proj2Groups):-
     write_groups(Str2, Proj2Groups),
     close(Str2).
 
+/**
+ * write_group(+Stream, +Group)
+ * 
+ * Stream - file stream
+ * Group - list of students that form a group
+ */ 
 write_group(Stream, []):-write(Stream, '\n').
-
 write_group(Stream, [H | T]):-
     write(Stream, H),
     write(Stream, '  '),
     write_group(Stream, T).
 
+/**
+ * write_groups(+Stream, +Groups)
+ * 
+ * Stream - file stream
+ * Groups - list of groups
+ */ 
 write_groups(_, []).
 write_groups(Stream, [H | T]):-
     write_group(Stream, H),
